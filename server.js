e9533fd6ff2c56ad7c7a6a98b66d70ebb6da4700
Gaurav Kumar Tripathi <83753529+Tripathigaurav02@users.noreply.github.com>
2023-04-18 09:14:20 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser, userLeave,
+    getRoomUsers} = require('./utils/users');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const botName = 'Chatify Bot';
+
+//Run when client connects
+io.on("connection", socket => {
+    socket.on('joinRoom', ({username, room})=>{
+        const user = userJoin(socket.id, username, room)
+        socket.join(user.room);
+
+//Welcome current user
+socket.emit('message', formatMessage(botName,"Welcome to Chatify!"));
+
+//Brodcast when a user connects
+socket.broadcast.to(user.room).emit('message', formatMessage(botName,`${user.username} joined the chat`));
+
+        // send user and room info
+        io.to(user.room).emit('roomUsers',{
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+    });
+    
+    //Listen for chatMessage
+    socket.on('chatMessage', msg => {
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).emit('message', formatMessage(user.username,msg));
+    });
+
+     //Brodcast when a user disconnects
+     socket.on('disconnect', () => {
+        const user =  userLeave(socket.id);
+
+        if(user){
+            io.to(user.room).emit('message', formatMessage(botName,`${user.username} left the chat`));
+
+            io.to(user.room).emit('roomUsers',{
+                room: user.room,
+                users: getRoomUsers(user.room)
+            });
+        }
+        
+    });
+});
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
